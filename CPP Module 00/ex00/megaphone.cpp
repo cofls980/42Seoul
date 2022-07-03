92a3d390ae286443576e920a6722c2ac5cc74526
cofls980 <qawse980@gmail.com>
2022-07-04 03:23:28 +0900

    modify function

--- CPP Module 00/ex00/megaphone.cpp
+++ CPP Module 00/ex00/megaphone.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
 		{
 			for (int j = 0;argv[i][j];j++)
 			{
-				std::cout << (char)toupper(argv[i][j]);
+				std::cout << (char)std::toupper(argv[i][j]);
 			}
 		}
 		std::cout << std::endl;
