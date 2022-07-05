e96e9aa298d80ea7923804f08bd26c1b62399679
cofls980 <qawse980@gmail.com>
2022-07-05 20:23:40 +0900

    fix constructor

--- CPP Module 02/ex01/Fixed.cpp
+++ CPP Module 02/ex01/Fixed.cpp
@@ -1,10 +1,10 @@
 #include "Fixed.hpp"
-#include <iostream>
 
-Fixed::Fixed()
+const int Fixed::bit = 8;
+
+Fixed::Fixed() : fixedPointValue(0)
 {
 	std::cout << "Default constructor called" << std::endl;
-	this->value = 0;
 }
 
 Fixed::Fixed(const Fixed& fRef)
@@ -16,7 +16,7 @@ Fixed::Fixed(const Fixed& fRef)
 Fixed& Fixed::operator=(const Fixed& fRef)
 {
 	std::cout << "Copy assignment operator called" << std::endl;
-	this->value = fRef.getRawBits();
+	this->fixedPointValue = fRef.getRawBits();
 	return *this;
 }
 
@@ -27,34 +27,34 @@ Fixed::~Fixed()
 
 int Fixed::getRawBits(void) const
 {
-	return (this->value);
+	return (this->fixedPointValue);
 }
 
 void Fixed::setRawBits(int const raw)
 {
-	this->value = raw;
+	this->fixedPointValue = raw;
 }
 
 Fixed::Fixed(const int value)
 {
 	std::cout << "Int constructor called" << std::endl;
-	this->value = value << this->bit;
+	this->fixedPointValue = value << this->bit;
 }
 
 Fixed::Fixed(const float value)
 {
 	std::cout << "Float constructor called" << std::endl;
-	this->value = roundf(value * (1 << this->bit));
+	this->fixedPointValue = roundf(value * (1 << this->bit));
 }
 
 float Fixed::toFloat(void) const
 {
-	return ((float)this->value / (float)(1 << this->bit));
+	return ((float)this->fixedPointValue / (float)(1 << this->bit));
 }
 
 int Fixed::toInt(void) const
 {
-	return (this->value >> this->bit);
+	return (this->fixedPointValue >> this->bit);
 }
 
 std::ostream& operator<<(std::ostream &out, const Fixed& fRef)
--- CPP Module 02/ex01/Fixed.hpp
+++ CPP Module 02/ex01/Fixed.hpp
@@ -6,13 +6,13 @@
 
 class Fixed {
 private:
-	int value;
-	const static int bit = 8;
+	int fixedPointValue;
+	const static int bit;
 
 public:
 	Fixed();
-	Fixed(const Fixed& fRef);//복사 생성자(Copy constructor)
-	Fixed& operator=(const Fixed& fRef);//복사 대입 연산자(Copy assignment operator)
+	Fixed(const Fixed& fRef);
+	Fixed& operator=(const Fixed& fRef);
 	~Fixed();
 	int getRawBits(void) const;
 	void setRawBits(int const raw);
@@ -21,8 +21,6 @@ public:
 	Fixed(const float value);
 	float toFloat(void) const;
 	int toInt(void) const;
-	//std::ostream& operator<<(std::ostream &out, const Fixed& fRef);
-	//std::ostream& operator << (std::ostream &out, const Fixed& fRef);
 };
 
 std::ostream& operator<<(std::ostream &out, const Fixed& fRef);
