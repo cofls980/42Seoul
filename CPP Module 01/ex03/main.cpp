88ce9921308cd98651d7d1c62d95913f1c01950a
cofls980 <qawse980@gmail.com>
2022-07-07 22:25:21 +0900

    debug

--- CPP Module 01/ex03/main.cpp
+++ CPP Module 01/ex03/main.cpp
@@ -5,6 +5,7 @@ int main()
 {
 	{
 		Weapon club = Weapon("crude spiked club");
+
 		HumanA bob("Bob", club);
 		bob.attack();
 		club.setType("some other type of club");
@@ -12,8 +13,9 @@ int main()
 	}
 	{
 		Weapon club = Weapon("crude spiked club");
+		
 		HumanB jim("Jim");
-		jim.setWeapon(club);
+		//jim.setWeapon(club);
 		jim.attack();
 		club.setType("some other type of club");
 		jim.attack();
